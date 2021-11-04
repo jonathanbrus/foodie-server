6e8de56f4387abf8f3c05998ed8616eba73b5b5b
jonathanbrus <jonathanbrus966@gmail.com>
2021-11-04 17:26:23 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -17,21 +17,21 @@ const Router = require("./router");
 
 App.use(Router);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_STRING, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    App.listen(
-      PORT,
-      "0.0.0.0" || "localhost",
-      console.log(`server running on port ${PORT}`)
-    );
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(process.env.MONGO_STRING, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+App.listen(
+  PORT,
+  "0.0.0.0" || "localhost",
+  console.log(`server running on port ${PORT}`)
+);
+// })
+// .catch((err) => {
+//   console.log(err);
+// });
